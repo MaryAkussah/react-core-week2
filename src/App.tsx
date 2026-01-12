15f07206d597e32e7022714d1886daf9bf23d98d
MaryAkussah <maryakussah123@gmail.com>
2026-01-12 23:44:51 +0000

    created a timer using useEffects

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Timer from './components/Timer'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+     <h1> Its week 2 actually!</h1>
+     <Timer />
+
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Timer.tsx
@@ -0,0 +1,19 @@
+import {useEffect,useState} from 'react';
+
+function Timer(){
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds((prev) => prev + 1)
+        }, 1000)
+
+        return () => clearInterval(interval);
+    },[])
+
+return <h3>Timer : {seconds}s</h3>
+    
+
+
+}
+export default Timer;
